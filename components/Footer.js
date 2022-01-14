269468fc3548a22ec43a659f9988db72cfa1a774
Joy <kamlomo.js@gmail.com>
2022-01-14 20:15:35 +0200

    fiixed footer flex

--- components/Footer.js
+++ components/Footer.js
@@ -1,91 +1,135 @@
 function Footer() {
     return (
-      <footer className="bg-gray-900 text-white body-font mt-12">
-      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-        <div className="w-80 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-          <a className="flex font-extrabold items-center md:justify-start justify-center text-lime-400">
-            <span className="text-xl uppercase">company name</span>
-          </a>
-          <p className="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
-        </div>
-        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-lime-500 tracking-widest text-sm mb-3">Information</h2>
-            <nav className="ml-2 list-none mb-10">
-              <li>
-                <a className="text-white hover:text-gray-800">First Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Second Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Fourth Link</a>
-              </li>
-            </nav>
+     
+      <footer class="bg-gray-900 mt-10" aria-labelledby="footer-heading">   
+      <div class="px-4 py-24 mx-auto max-w-7xl sm:px-6 lg:px-16">
+        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+          <div class="text-white xl:col-span-1">
+            <a href="/" class="
+          text-lg
+          font-bold
+          tracking-tighter
+          text-lime-500
+          transition
+          duration-500
+          ease-in-out
+          transform
+          tracking-relaxed
+          lg:pr-8
+        "> Auto Accessories Vessel </a>
+            <p class="w-1/2 mt-2 text-sm text-gray-500"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque </p>
           </div>
-          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-lime-500 tracking-widest text-sm mb-3">Customer Service</h2>
-            <nav className="ml-2 list-none mb-10">
-              <li>
-                <a className="text-white hover:text-gray-800">First Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Second Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Fourth Link</a>
-              </li>
-            </nav>
-          </div>
-          <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-lime-500 tracking-widest text-sm mb-3">Contact us </h2>
-            <nav className="ml-2 list-none mb-10">
-              <li>
-                <a className="text-white hover:text-gray-800">First Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Second Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Third Link</a>
-              </li>
-              <li>
-                <a className="text-white hover:text-gray-800">Fourth Link</a>
-              </li>
-            </nav>
+          <div class="grid grid-cols-2 gap-8 mt-12 xl:mt-0 xl:col-span-2">
+            <div class="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 class="text-sm font-bold tracking-wider text-lime-500 uppercase"> INFORMATION </h3>
+                <ul role="list" class="mt-4 space-y-2">
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> Pricing </a>
+                  </li>
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> Delivery Info </a>
+                  </li>
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> Paying by Invoice </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="mt-12 md:mt-0">
+                <h3 class="text-sm font-bold tracking-wider text-lime-500 uppercase"> customer service </h3>
+                <ul role="list" class="mt-4 space-y-2">
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> Rreturn an order </a>
+                  </li>
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> Orders and Returns </a>
+                  </li>
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> FAQs </a>
+                  </li>
+                  <li>
+                    <a href="/pricing" class=" text-base font-normal text-gray-500 hover:text-lime-500"> Contact us </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 class="text-sm font-bold tracking-wider text-lime-500 uppercase"> Legal </h3>
+                <ul role="list" class="mt-4 space-y-2">
+                  <li>
+                    <a href="./refund.html" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> Refund </a>
+                  </li>
+                  <li>
+                    <a href="/license" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> License </a>
+                  </li>
+                  <li>
+                    <a href="/privacy" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> Privacy Policy </a>
+                  </li>
+                  <li>
+                    <a href="/terms" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> Terms </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="mt-12 md:mt-0">
+                <h3 class="text-sm font-bold tracking-wider text-lime-500 uppercase"> Socials </h3>
+                <ul role="list" class="mt-4 space-y-2">
+                  <li>
+                    <a href="https://twitter.com/" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> Twitter </a>
+                  </li>
+                  <li>
+                    <a href="https://facebook.com/" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> Facebook </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/" class="
+                  text-base
+                  font-normal
+                  text-gray-500
+                  hover:text-lime-500
+                "> Indie Hackers </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="bg-gray-50">
-        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-          <p className="text-lime-500 text-sm text-center sm:text-left">© 2020 company name —
-            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-white ml-1" target="_blank">@knyttneve</a>
-          </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a className="text-blue-500">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a className="ml-3 text-blue-400">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a className="ml-3 text-indigo-600">
-              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-              </svg>
-            </a>
-          </span>
+      <div className="bg-gray-50"> 
+      <div class="px-4 py-8 mx-auto max-w-7xl sm:px-6 lg:px-16">
+        <div class="flex flex-wrap items-baseline">
+          <span class="mt-2 text-sm font-light text-gray-500"> Copyright © 2022 <a href="https://autoaccessoriesvessel.dev" class="mx-2 text-wickedlime hover:text-gray-500" rel="noopener noreferrer">@Auto Accessories Vessel</a>. Since 2020 </span>
         </div>
+      </div>      
       </div>
     </footer>
     )
--- components/LogoCloud.js
+++ components/LogoCloud.js
@@ -1,7 +1,7 @@
 function LogoCloud() {
     return (
         <section className="mx-w-7xl mx-auto">
-        <div className="container flex flex-wrap -mx-2 overflow-hidden sm:-mx-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
+        <div className="w-full flex flex-wrap md:ml-5 lg:ml-5 xl:ml:5 -mx-2 overflow-hidden sm:-mx-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
 
         <div className="my-2 px-2 w-1/6 overflow-hidden sm:my-2 sm:px-2 sm:w-1/6 md:my-2 md:px-2 md:w-1/6 lg:my-2 lg:px-2 lg:w-1/6 xl:my-2 xl:px-2 xl:w-1/6">
         <img className="mx-auto h-32" src="https://img.icons8.com/color/500/000000/bmw--v1.png" alt="" />
